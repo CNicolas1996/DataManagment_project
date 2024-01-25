64aa25d356e19b9815fcff4ec32dc6aefaa9d85c
Nicolas <akratalopez77@gmail.com>
2024-01-25 01:49:12 -0500

    Guardar cambios locales en Extraction.py

--- src/Extraction.py
+++ src/Extraction.py
@@ -3,7 +3,9 @@ import chardet
 import codecs
 import os
 import numpy as np
-
+from IPython.display import display
+import matplotlib.pyplot as plt
+import missingno as msno
 
 def correct_bad_line(bad_line, column_names):
     # Reemplaza los campos adicionales con valores nulos
@@ -91,12 +93,11 @@ class DataProcessor:
             print(f"Error al detectar información del archivo CSV: {str(e)}")
 
     def convert_to_utf8(self):
-
         """
-        Convierte el archivo a formato UTF-8.
+        Convierte el archivo a formato UTF-8 si no está en ese formato.
         """
         try:
-            if self.detected_encoding.lower() != 'utf-8':
+            if self.detected_encoding.lower() != "utf-8-sig":
                 # Convertir el archivo a utf-8 usando codecs
                 with codecs.open(self.file_path, 'r', encoding=self.detected_encoding, errors='replace') as source_file:
                     with codecs.open(self.converted_file_path, 'w', encoding='utf-8') as target_file:
@@ -109,24 +110,25 @@ class DataProcessor:
             print(f"Error durante la conversión a UTF-8: {str(e)}")
             print(f"Archivo con error: {self.file_path}")
 
+        return self.converted_file_path if os.path.exists(self.converted_file_path) and self.converted_file_path != self.file_path else self.file_path
 
-    def read_csv_summary(self, chunk_size=None, usecols=None, skiprows=None, sep=','):
 
-        """
-        Lee el archivo CSV en chunks y devuelve un resumen y los chunks para procesamiento posterior.
-        """
+    def read_csv_summary(self, chunk_size=None, usecols=None, skiprows=None, sep=',', header=None):
+
         try:
             if self.detected_encoding is None:
                 self.detect_file_info()
 
-            self.convert_to_utf8()
+            # Realiza la conversión solo si es necesario
+            file_path_to_read = self.file_path
+            if self.detected_encoding.lower() != 'utf-8':
+                file_path_to_read = self.convert_to_utf8()
 
-            file_path_to_read = self.converted_file_path or self.file_path
             print(f"Intentando leer el archivo convertido: {file_path_to_read}")
 
             if chunk_size is None:
                 self.data = pd.read_csv(file_path_to_read, usecols=usecols, skiprows=skiprows, sep=sep,
-                                        encoding='utf-8', on_bad_lines='warn')
+                                        encoding='utf-8', on_bad_lines='warn', header=header)
 
                 print(f"Lectura exitosa del archivo CSV.")
                 print(f"- Número de filas: {len(self.data)}")
@@ -135,72 +137,78 @@ class DataProcessor:
                 return self.data  # Devuelve el DataFrame completo
 
             else:
+                # Resto del código para procesar chunks
                 chunks_info = []  # Lista para almacenar información de los chunks
                 chunks = pd.read_csv(file_path_to_read, chunksize=chunk_size, usecols=usecols, skiprows=skiprows,
-                                     sep=sep, encoding='utf-8', on_bad_lines='warn')
+                                     sep=sep, encoding='utf-8', on_bad_lines='warn', header=header)
 
                 for i, chunk in enumerate(chunks):
-                    # Procesa cada chunk
-                    chunk_info = {
-                        "Chunk": i + 1,
-                        "Número de filas": len(chunk),
-                        "Número de columnas": len(chunk.columns),
-                    }
-                    chunks_info.append(chunk_info)
-                if chunk.shape[1] != self.column_names:
-                    print(f"Error en la línea {i + 1}: número de columnas incorrecto.")
+                    try:
+                        # Procesa cada chunk
+                        chunk_info = {
+                            "Chunk": i + 1,
+                            "Número de filas": len(chunk),
+                            "Número de columnas": len(chunk.columns),
+                        }
+                        chunks_info.append(chunk_info)
 
+                        # Verifica la cantidad de columnas en el chunk actual
+                        if chunk.shape[1] != len(self.column_names):
+                            print(f"Error en el Chunk {i + 1}: número de columnas incorrecto.")
+
+                    except pd.errors.ParserError as pe_chunk:
+                        print(f"Error al leer el Chunk {i + 1}: {str(pe_chunk)}")
+                        # Puedes manejar este error de alguna manera específica si es necesario
 
                 # Convierte la lista de información de chunks a DataFrame
                 chunks_summary = pd.DataFrame(chunks_info)
 
-
                 # Reinicia la lectura para procesar los chunks posteriormente
                 chunks = pd.read_csv(file_path_to_read, chunksize=chunk_size, usecols=usecols, skiprows=skiprows,
-                                     sep=sep, encoding='utf-8', on_bad_lines='warn')
+                                     sep=sep, encoding='utf-8', on_bad_lines='warn', header=header)
 
                 return chunks_summary, None, chunks  # Devuelve el resumen de chunks y los chunks para su procesamiento posterior :)
 
         except pd.errors.ParserError as pe:
             print(f"Error al leer el archivo CSV: {str(pe)}")
 
-            # Intenta leer el archivo reemplazando el error_bad_lines con False para llenar los campos faltantes con NaN
-            chunks = pd.read_csv(file_path_to_read, chunksize=chunk_size, usecols=usecols, skiprows=skiprows,
-                                 sep=sep, encoding='utf-8', on_bad_lines='warn', error_bad_lines=False)
-
-            chunks_info = []  # Lista para almacenar información de los chunks
-            for i, chunk in enumerate(chunks):
-                # Procesa cada chunk
-                chunk_info = {
-                    "Chunk": i + 1,
-                    "Número de filas": len(chunk),
-                    "Número de columnas": len(chunk.columns),
-                }
-                chunks_info.append(chunk_info)
-            if chunk.shape[1] != self.column_names:
-                print(f"Error en la línea {i + 1}: número de columnas incorrecto.")
-
-
-            # Convierte la lista de información de chunks a DataFrame
             chunks_summary = pd.DataFrame(chunks_info)
 
             return chunks_summary, None, chunks
 
-
-
+#
 if __name__ == "__main__":
-    csv_path = r"C:\Repositorios\DataManagment_project\TestData\BASE_DE_DATOS_DE_EMPRESAS_Y_O_ENTIDADES_ACTIVAS_-_JURISDICCI_N_C_MARA_DE_COMERCIO_DE_IBAGU__-_CORTE_A_31_DE_AGOSTO_DE_2023.csv"
+    from Transform import DataTransformer
 
+    csv_path = r"C:\Repositorios\DataManagment_project\TestData\BASE_DE_DATOS_DE_EMPRESAS_Y_O_ENTIDADES_ACTIVAS_-_JURISDICCI_N_C_MARA_DE_COMERCIO_DE_IBAGU__-_CORTE_A_31_DE_AGOSTO_DE_2023_converted_utf-8.csv"
     data_processor = DataProcessor(csv_path)
-    chunks_summary, _, chunks = data_processor.read_csv_summary(chunk_size=3000)
+    data = data_processor.read_csv_summary(header=0)
+    transformer = DataTransformer(data)
+    df_missing_values, cols_with_missing_values = transformer.analyze_missing_values()
+    print(df_missing_values.to_string())
+    print(cols_with_missing_values)
 
-for i, chunk in enumerate(chunks):
-    print("Resumen de los chunks:")
-    print(chunks_summary)
-    concatenated_data = pd.concat(chunks, ignore_index=True)
-    print("Concatenación de todos los chunks: \n\n")
-    print(f"Info de los chunks concatenados: \n\n {concatenated_data.info()}")
-    print(concatenated_data.sample(30))
+    most_frequent_values_info = df_missing_values.loc[:, ['Data Type', 'Most Frequent Value']]
+
+    # Seleccionar solo algunas columnas (por ejemplo, las columnas 'NACIONALIDAD' y 'FECHA DE VIGENCIA')
+    for column in cols_with_missing_values:
+        # Manejar valores nulos utilizando el método 'fill' con fill_method='mode'
+        info = transformer.handle_missing_values(method='fill', fill_method='mode', columns=[column])
+
+        # Verificar si info no es None antes de imprimir
+        if info is not None:
+            print(info.to_string())
+        else:
+            print(f"No hay información para la columna {column}")
+
+    # Llenar automáticamente los valores nulos en todas las columnas con los valores más frecuentes
+    for column in most_frequent_values_info.index:
+        data[column] = data[column].fillna(most_frequent_values_info.loc[column, 'Most Frequent Value'])
+
+    # Imprimir el DataFrame después de llenar los valores nulos
+    msno.matrix(data)
+    plt.savefig('null_after_fill.png')
+    plt.show()
 
 
 
---
+++ src/Transform.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import display
+
+class DataTransformer:
+    def __init__(self, data):
+        self.data = data
+        self.null_handling_results = pd.DataFrame()
+
+    def visualize_null_data(self):
+        if isinstance(self.data, pd.io.parsers.TextFileReader):
+            # Concatenar todos los chunks en un solo DataFrame
+            concatenated_data = pd.concat(self.data, ignore_index=True)
+
+            # Configurar el estilo de seaborn (opcional)
+            sns.set(style="darkgrid")
+
+            # Visualizar la matriz de nulos para el DataFrame completo con Seaborn
+            plt.figure(figsize=(20, 15))
+            ax = sns.heatmap(concatenated_data.isnull(), cbar=False, cmap="Blues", yticklabels=False)
+
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha="center")
+            plt.title("Visualización de Nulos - DataFrame Completo")
+            self.fig = plt.gcf()  # Obtener la figura actual
+            plt.close()  # Cerrar la ventana de visualización
+
+        elif isinstance(self.data, pd.DataFrame):
+            # Configurar el estilo de seaborn (opcional)
+            sns.set(style="whitegrid")
+
+            # Visualizar la matriz de nulos para el DataFrame completo con Seaborn
+            plt.figure(figsize=(20, 15))
+            ax = sns.heatmap(self.data.isnull(), cbar=False, cmap="Blues", yticklabels=False)
+
+            # Girar los labels 90 grados y ubicarlos de arriba a abajo
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha="center")
+            plt.title("Visualización de Nulos - DataFrame Completo")
+            self.fig = plt.gcf()  # Obtener la figura actual
+            plt.close()  # Cerrar la ventana de visualización
+
+        else:
+            raise ValueError("El tipo de datos no es compatible.")
+
+    def save_visualization(self, filename="visualization.png"):
+        if self.fig:
+            self.fig.savefig(filename)
+            print(f"La visualización se ha guardado en {filename}")
+        else:
+            print("No se ha generado ninguna visualización.")
+
+    def analyze_missing_values(self):
+        if isinstance(self.data, pd.io.parsers.TextFileReader):
+            # Analizar valores nulos para cada chunk
+            results = {}
+            for i, chunk in enumerate(self.data):
+                chunk_info = self._get_chunk_info(chunk)
+                results[f'Chunk {i + 1}'] = chunk_info
+            return results
+        elif isinstance(self.data, pd.DataFrame):
+            # Analizar valores nulos para el DataFrame completo
+            detailed_report = self._get_chunk_info(self.data)
+            return detailed_report
+        else:
+            raise ValueError("El tipo de datos no es compatible.")
+
+    def _get_chunk_info(self, chunk):
+        null_counts = chunk.isnull().sum()
+        columns_with_nulls = null_counts[null_counts > 0].index.tolist()
+
+        # Crear DataFrame con información sobre valores nulos
+        chunk_info = pd.DataFrame({'Null Count': null_counts[columns_with_nulls]})
+
+        # Agregar columna con porcentaje de valores nulos
+        chunk_info['Null Percentage'] = chunk_info['Null Count'] / len(chunk) * 100
+
+        # Agregar columna con el tipo de datos de cada columna
+        chunk_info['Data Type'] = chunk.dtypes[columns_with_nulls]
+
+        # Agregar columna con el valor más frecuente (moda) y su frecuencia
+        chunk_info['Most Frequent Value'] = chunk.mode().iloc[0][columns_with_nulls]
+        chunk_info['Frequency'] = chunk.apply(
+            lambda col: col.value_counts().iloc[0] if col.name in columns_with_nulls else None)
+
+        # Establecer la primera columna como índice y darle un título
+        chunk_info.index.name = 'Column'
+        chunk_info.columns.name = 'Missing Values Report'
+
+        return chunk_info, columns_with_nulls
+
+    import pandas as pd
+
+    def fill_missing_values(self, fill_method='ffill'):
+        """
+        Llena los valores faltantes en el DataFrame utilizando el método especificado.
+
+        Parámetros:
+        - fill_method: str, el método de llenado a utilizar ('ffill', 'mode', o 'bfill').
+
+        Retorna:
+        - DataFrame, el DataFrame con los valores faltantes llenados.
+        """
+        if fill_method == 'mode':
+            # Llenar utilizando la moda para todas las columnas
+            self.data = self.data.apply(lambda x: x.fillna(x.mode().iat[0]))
+            print("Valores nulos llenados en el DataFrame usando moda.")
+        elif fill_method == 'ffill':
+            # Llenar hacia adelante
+            self.data = self.data.ffill()
+        elif fill_method == 'bfill':
+            # Llenar hacia atrás
+            self.data = self.data.bfill()
+        else:
+            # Si el método no es reconocido, mostrar un mensaje de error
+            raise ValueError("Método de llenado no reconocido. Use 'ffill', 'mode', o 'bfill'.")
+
+        return self.data
+
+    def handle_missing_values(self, method='drop', columns=None, global_handling=False, fill_method=None):
+        if columns is None:
+            columns = self.data.columns
+
+        if global_handling:
+            print(f"Applying global handling for method: {method}")
+            if method == 'drop':
+                # Almacena resultados antes de manejar los valores nulos
+                before_handling = self.data[columns].isnull().sum().rename('Before Handling')
+
+                # Aplica lógica de manejo de valores nulos al DataFrame completo
+                self.data.dropna(subset=columns, inplace=True)
+
+                # Almacena resultados después de manejar los valores nulos
+                after_handling = self.data[columns].isnull().sum().rename('After Handling')
+
+                # Actualiza el registro de resultados
+                self.null_handling_results = pd.concat([self.null_handling_results, before_handling, after_handling],
+                                                       axis=1)
+                print(f"Valores nulos manejados en el DataFrame para las columnas {columns}")
+            elif method == 'fill':
+                if fill_method:
+                    self.fill_missing_values(fill_method)
+                else:
+                    print("Debe especificar un método de llenado para 'fill'.")
+
+                # Almacena resultados después de manejar los valores nulos
+                after_handling = self.data[columns].isnull().sum().rename('After Handling')
+                # Actualiza el registro de resultados
+                self.null_handling_results = pd.concat([self.null_handling_results, after_handling], axis=1)
+            elif callable(method):
+                # Almacena resultados antes de manejar los valores nulos
+                before_handling = self.data[columns].isnull().sum().rename('Before Handling')
+
+                # Si se proporciona una función, aplica la función al DataFrame completo
+                self.data[columns] = self.data[columns].apply(method)
+
+                # Almacena resultados después de manejar los valores nulos
+                after_handling = self.data[columns].isnull().sum().rename('After Handling')
+
+                # Actualiza el registro de resultados
+                self.null_handling_results = pd.concat([self.null_handling_results, before_handling, after_handling],
+                                                       axis=1)
+                print(f"Valores nulos manejados en el DataFrame para las columnas {columns}")
+            else:
+                print("Método no válido. Use 'drop', 'fill', o una función personalizada.")
+
+            # Devuelve información que pueda ser útil
+            return self.null_handling_results
+
+
+
+
+
+
+
+
+
+
